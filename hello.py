b813a33936bb553465242e85d96e7ec6ce882cbb
caser <jiao.xuejiao@gmail.com>
2015-07-26 15:31:40 +0800

    init

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 # -*- coding:utf-8 -*-
+import os
 from flask import Flask
 app = Flask(__name__)
 
@@ -6,5 +7,3 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, world'
 
-if __name__=='__main__':
-    app.run()
